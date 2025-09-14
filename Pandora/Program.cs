e78b18c434f8285ff94a128651a6e9649da503d8
EqUiNoX <equinox.teamresurgent@gmail.com>
2025-09-14 15:43:00 -0600

    added crash log

--- Pandora/Program.cs
+++ Pandora/Program.cs
@@ -1,17 +1,50 @@
 ﻿using Avalonia;
 using Avalonia.ReactiveUI;
 using System;
+using System.IO;
+using System.Threading.Tasks;
 
 namespace Pandora
 {
     internal sealed class Program
     {
-        // Initialization code. Don't use any Avalonia, third-party APIs or any
-        // SynchronizationContext-reliant code before AppMain is called: things aren't initialized
-        // yet and stuff might break.
+        private static void LogException(string type, Exception? ex)
+        {
+            try
+            {
+                var logPath = Path.Combine(AppContext.BaseDirectory, "crashlog.txt");
+                File.AppendAllText(logPath, $"[{DateTime.Now:yyyy-MM-dd HH:mm:ss}] {type}: {ex?.ToString() ?? "Unknown error"}{Environment.NewLine}");
+            }
+            catch
+            {
+                // Ignore logging failures
+            }
+        }
+
         [STAThread]
-        public static void Main(string[] args) => BuildAvaloniaApp()
-            .StartWithClassicDesktopLifetime(args);
+        public static void Main(string[] args)
+        {
+            AppDomain.CurrentDomain.UnhandledException += (sender, e) =>
+            {
+                LogException("UnhandledException", e.ExceptionObject as Exception);
+            };
+
+            TaskScheduler.UnobservedTaskException += (sender, e) =>
+            {
+                LogException("UnobservedTaskException", e.Exception);
+                e.SetObserved();
+            };
+
+            try
+            {
+                BuildAvaloniaApp().StartWithClassicDesktopLifetime(args);
+            }
+            catch (Exception ex)
+            {
+                LogException("FatalException", ex);
+                throw; 
+            }
+        }
 
         // Avalonia configuration, don't remove; also used by visual designer.
         public static AppBuilder BuildAvaloniaApp()
